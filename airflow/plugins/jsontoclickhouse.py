8bc9eff222f1557bb21cd56b83b4c1a3c85a20fb
care1e55 <sfilms89@gmail.com>
2020-05-04 00:51:30 +0300

    without templating

--- airflow/plugins/jsontoclickhouse.py
+++ airflow/plugins/jsontoclickhouse.py
@@ -4,22 +4,24 @@ from airflow.utils.decorators import apply_defaults
 from sqlalchemy import create_engine
 import re
 
-class JsonToClickhouse(BaseOperator):
+class JsonToClickhouseOperator(BaseOperator):
+
+    template_fields = ['host', 'schema', 'table', 'filepath', 'jsonname']
 
     @apply_defaults
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def filterJSON(line):
+    def filterJSON(self, line):
         re.sub(":([0-9]+?),", ":\"\1\",", line)
         re.sub(":([0-9\.]+?)}", ":\"\1\"}", line)
         return line
 
     def execute(self, context):
-        uri = 'clickhouse://default:@localhost:8123'
+        uri = 'clickhouse://default:@clickhouse:8123'
         engine = create_engine(uri)
 
-        with open("./airflow/files/event-data-head.json", "r") as fi:
+        with open("/files/event-data-head.json", "r") as fi:
             for line in map(self.filterJSON, fi.readlines()):
                 sql = r'INSERT INTO default.fromjson  FORMAT JSONEachRow ' + line
                 engine.execute(sql)
